9836da5328f0336046a5e6c9ab7f7b7080280717
Nick Garbalau <nick.g@worldviewltd.com>
2024-07-08 16:53:45 +0200

    fix: add scaffold ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -9,12 +9,24 @@ export const metadata: Metadata = {
   icons: [{ rel: "icon", url: "/favicon.ico" }],
 };
 
+const TopNav = () => {
+    return (
+        <nav className="flex items-center justify-between p-4 text-xl font-semibold border-b">
+            <div>Gallery</div>
+            <div>Sign In</div>
+        </nav>
+    )
+} 
+
 export default function RootLayout({
   children,
 }: Readonly<{ children: React.ReactNode }>) {
   return (
     <html lang="en" className={`${GeistSans.variable}`}>
-      <body>{children}</body>
+        <body className="font-sans flex flex-col gap-4">
+            <TopNav />
+            {children}
+        </body>
     </html>
   );
 }
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,29 @@
 import Link from "next/link";
 
+const mockUrls = [
+    'https://utfs.io/f/ac6727bf-0374-4c8f-8085-eeb5917f6e07-tk0933.png',
+    'https://utfs.io/f/5275cf01-abf2-4cd0-b3a1-7e00a2c7513d-2tno26.png',
+    'https://utfs.io/f/84e13b1d-746d-4b45-9bf2-def2f4217718-v2srne.png',
+    'https://utfs.io/f/2a3c158b-ca06-4272-b730-8af89ac383ef-6ktaa6.png'
+]
+
+const mockImages = mockUrls.map((url, index) => ({
+    id: index + 1,
+    url
+}));
+
 export default function HomePage() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-        Hello (gallery in progress)
+    <main>
+        <div className="flex flex-wrap gap-4">
+            {[...mockImages, ...mockImages, ...mockImages].map((image) => {
+                return (
+                    <div key={image.id} className="w-48">
+                        <img src={image.url} alt="Image"/>
+                    </div>
+                )
+            })}
+        </div>
     </main>
   );
 }
